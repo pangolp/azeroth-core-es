3170012aa87509bef600b20acf6f62945265553f
Pagani Walter <paganiwalter@gmail.com>
2019-10-06 03:24:15 -0300

    Agregando del 103 al 197

---
+++ creature_text_locale/creature_103_197.sql
@@ -0,0 +1,23 @@
+DELETE FROM `creature_text_locale` WHERE `Locale` IN ('esES', 'esMX') AND `CreatureID` IN (103, 116, 117, 121, 122, 123, 124, 125, 197);
+
+INSERT INTO `creature_text_locale` (`CreatureID`, `GroupID`, `ID`, `Locale`, `Text`) VALUES
+(103,0,0,'esES','Veo que esos tontos de la Abadía nos enviaron carne fresca.'),
+(103,0,1,'esES','¡Esta tierra pertenece a la Hermandad de los Delfias ahora!'),
+(116,0,0,'esES','La Hermandad no tolerará tus acciones.'),
+(116,0,1,'esES','Ah, la oportunidad de usar esta hoja recién afilada.'),
+(116,0,2,'esES','Siente el poder de la Hermandad!'),
+(117,0,0,'esES','Más huesos para roer...'),
+(117,0,1,'esES','Grrrrrr... ¡carne fresca!'),
+(121,0,0,'esES','La Hermandad no tolerará tus acciones.'),
+(121,0,1,'esES','Ah, la oportunidad de usar esta hoja recién afilada.'),
+(121,0,2,'esES','Siente el poder de la Hermandad!'),
+(122,0,0,'esES','La Hermandad no tolerará tus acciones.'),
+(122,0,1,'esES','Ah, la oportunidad de usar esta hoja recién afilada.'),
+(122,0,2,'esES','Siente el poder de la Hermandad!'),
+(123,0,0,'esES','Más huesos para roer...'),
+(123,0,1,'esES','Grrrrrr... ¡carne fresca!'),
+(124,0,0,'esES','Más huesos para roer...'),
+(124,0,1,'esES','Grrrrrr... ¡carne fresca!'),
+(125,0,0,'esES','Más huesos para roer...'),
+(125,0,1,'esES','Grrrrrr... ¡carne fresca!'),
+(197,0,0,'esES','Puedes retirarte, $n.');
--- creature_text_locale/creature_6_98.sql
+++ creature_text_locale/creature_6_98.sql
@@ -1,28 +1,21 @@
 DELETE FROM `creature_text_locale` WHERE `Locale` IN ('esES', 'esMX') AND `CreatureID` IN (6, 29, 38, 40, 54, 61, 68, 80, 95, 97, 98);
 
 INSERT INTO `creature_text_locale` (`CreatureID`, `GroupID`, `ID`, `Locale`, `Text`) VALUES
--- Alimaña kóbold
 (6,0,0,'esES','¡Tú no llevas vela!'),
 (6,0,1,'esES','¡Yujuuuuuu! ¡Yo correr!'),
 (6,0,2,'esES','¡No, mátame! ¡No, mátame!'),
--- Dragon Spawn
 (29,1,0,'esES','¡Que comience la batalla por el filamento de los Antiguos!'),
 (29,2,0,'esES','Ronda 1 - ¡Terminado!'),
--- Defias Thug
 (38,0,0,'esES','La Hermandad no tolerará tus acciones.'),
 (38,0,1,'esES','Ah, la oportunidad de usar esta hoja recién afilada.'),
 (38,0,2,'esES','¡Siente el poder de la Hermandad!'),
--- Kobold Miner
 (40,0,0,'esES','¡Tú no llevas vela!'),
 (40,0,1,'esES','¡Yujuuuuuu! ¡Yo correr!'),
 (40,0,2,'esES','¡No, mátame! ¡No, mátame!'),
--- Corina Steele <Weaponsmith>
 (54,0,0,'esES','Saludos $N!'),
--- Thuros Lightfingers
 (61,0,0,'esES','La Hermandad no tolerará tus acciones.'),
 (61,0,1,'esES','Ah, la oportunidad de usar esta hoja recién afilada.'),
 (61,0,2,'esES','Siente el poder de la Hermandad!'),
--- Stormwind City Guard
 (68,0,0,'esES','¡Prueba la hoja, mestizo!'),
 (68,0,1,'esES','Por favor, dime que no hiciste lo que creo que acabas de hacer. Por favor, dime que no voy a tener que hacerte daño...'),
 (68,0,2,'esES','Como si no tuviéramos suficientes problemas, ¡ustedes van y crean más!'),
@@ -37,17 +30,13 @@ INSERT INTO `creature_text_locale` (`CreatureID`, `GroupID`, `ID`, `Locale`, `Te
 (68,5,5,'esES','Parece que vamos a tener una ejecución.'),
 (68,5,6,'esES','Perro traidor.'),
 (68,5,7,'esES','¡Mi familia fue aniquilada por el Azote! ¡MONSTRUO!'),
--- Kobold Laborer
 (80,0,0,'esES','¡Tú no llevas vela!'),
 (80,0,1,'esES','¡Yujuuuuuu! ¡Yo correr!'),
 (80,0,2,'esES','¡No, mátame! ¡No, mátame!'),
--- Defias Smuggler
 (95,0,0,'esES','La Hermandad no tolerará tus acciones.'),
 (95,0,1,'esES','Ah, la oportunidad de usar esta hoja recién afilada.'),
 (95,0,2,'esES','Siente el poder de la Hermandad!'),
--- Riverpaw Runt
 (97,0,0,'esES','Más huesos para roer...'),
 (97,0,1,'esES','Grrrrrr... ¡carne fresca!'),
--- Riverpaw Taskmaster
 (98,0,0,'esES','Más huesos para roer...'),
 (98,0,1,'esES','Grrrrrr... ¡carne fresca!');
